8c112409c5948eb75e56e24678619ba0e3edc4a8
JordiPan <jordipan.jp@gmail.com>
2023-12-29 17:42:14 +0100

    meer gesorteerd met mappen en modellen

---
+++ Modellen/Identity/Log.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Accessibility_app.Models
+{
+	[Table("Log")]
+	public class Log
+    {
+		public int Id { get; set; }
+		public string Categorie { get; set; }
+        public DateTime Tijdstempel { get; set; }
+        public string IP_adres { get; set; }
+        public string Beschrijving { get; set; }
+    }
+}
+
---
+++ Modellen/Registreermodellen/registerDeveloper.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Accessibility_backend.Modellen.Registreermodellen
+{
+    public class RegisterDeveloper
+    {
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string? Wachtwoord { get; set; }
+    }
+}
