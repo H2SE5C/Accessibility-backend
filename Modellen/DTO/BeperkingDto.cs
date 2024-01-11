d585760256ebca036838a3f15eb0edd2cf1657e5
Xavier8L <shenglongchen99@gmail.com>
2024-01-11 13:11:44 +0100

    onderzoek api add en late zie afgemaakt

---
+++ Modellen/DTO/BeperkingDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Accessibility_app.Models
+{
+	public class BeperkingDto
+    {
+		public int Id { get; set; }
+        public string Naam { get; set; }
+	}
+}
+
---
+++ Modellen/DTO/deskundigeEmailDto.cs
@@ -0,0 +1,11 @@
+﻿using Accessibility_app.Models;
+
+namespace Accessibility_backend.Modellen.Registreermodellen
+{
+	public class deskundigeEmailDto
+	{
+        public int Id { get; set; }
+        public string Email { get; set; }
+       
+	}
+}
