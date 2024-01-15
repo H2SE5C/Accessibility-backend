c49e087b776cb10c3d7889e7e281dfafe3f2d18d
Xavier8L <shenglongchen99@gmail.com>
2024-01-15 10:22:43 +0100

    methode getBeperkingen gemaakt

--- Controllers/OnderzoekController.cs
+++ Controllers/OnderzoekController.cs
@@ -164,6 +164,15 @@ namespace Accessibility_app.Controllers
             }
         }
 
+
+        [HttpGet("Beperkingen")]
+        public async Task<IActionResult> GetBeperkingen()
+        {
+            var beperkingen = await _context.Beperkingen.ToListAsync();
+
+            return Ok(beperkingen);
+        }
+
         // PUT api/<OnderzoekController>/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] OnderzoekForm onderzoeker)
