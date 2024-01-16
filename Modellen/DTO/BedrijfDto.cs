3543a3ef0e016eb28d56a9ea5c149b137fa77a92
tinepangsiu <tinepangsiu@gmail.com>
2024-01-16 23:46:52 +0100

    email username change

--- Modellen/DTO/BedrijfDto.cs
+++ Modellen/DTO/BedrijfDto.cs
@@ -4,11 +4,13 @@
 	public class BedrijfDto 
     {
         public int Id { get; set; }
+        public string UserName { get; set; }
         public string Bedrijfsnaam { get; set; }
         public string Omschrijving { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string Locatie { get; set; }
         public string LinkNaarBedrijf { get; set; }
+        public string CurrentPassword { get; set; }
     }
 }
