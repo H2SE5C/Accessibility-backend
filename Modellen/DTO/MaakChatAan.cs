4489a88098ac1f198395f225a5154eb1d7836f50
JordiPan <jordipan.jp@gmail.com>
2024-01-17 01:10:05 +0100

    begin gemaakt met chat en medewerker list verwijdert

---
+++ Modellen/DTO/MaakChatAan.cs
@@ -0,0 +1,10 @@
+﻿namespace Accessibility_backend.Modellen.DTO
+{
+	public class MaakChatAan
+	{
+		public int? OnderzoekId { get; set; }
+		public int? Gebruiker1 { get; set; }
+		public int? Gebruiker2 { get; set; }
+
+	}
+}
