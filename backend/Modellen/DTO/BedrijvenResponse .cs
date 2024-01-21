4654469e190a7c98ff8c6784ed79623ae292fda1
Xavier8L <shenglongchen99@gmail.com>
2024-01-21 15:59:22 +0100

    add bedrijven response

---
+++ backend/Modellen/DTO/BedrijvenResponse .cs
@@ -0,0 +1,10 @@
+﻿using Accessibility_app.Models;
+
+namespace Accessibility_backend.Modellen.DTO
+{
+    public class BedrijvenResponse
+    {
+        public List<Bedrijf> bedrijvenTrue { get; set; }
+        public List<Bedrijf> bedrijvenFalse { get; set; }
+    }
+}
